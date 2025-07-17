ca2687eb190d3e890d31026d70a0bc9bbce4e3d5
David Carrasco <davidack123456789@gmail.com>
2025-07-16 21:46:05 -0500

    actualizacion

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_ATLAS_URI);
+    await mongoose.connect("mongodb+srv://david20:bit2025@cluster0.ae4ssad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Atlas conectado");
   } catch (error) {
     console.log("Monfo BD Atlas no se pudo conectar", error);
@@ -10,3 +10,5 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
